32b8a1943437e6b021a2d80495c89b8cdd9258b8
Amit Kumar <2292amit@gmail.com>
2021-05-15 19:17:06 +0530

    adding contact table

--- client/src/ContactView.js
+++ client/src/ContactView.js
@@ -5,11 +5,46 @@ const ContactView = ({ children }) => {
       <>
           <div class="container p-3 my-3 border rounded bg-light text-dark">
                 <div class="text-center">
-                <h1>How to reach us?</h1>
-                <h4>Address: GT Rd, Gopalmath, Durgapur, West Bengal</h4>
-                <h4>Email: xxxx@xxx.com</h4>
-                <h4>Direct Call: 91xxxxxx89</h4>
-                <h4>Whatsapp: 91xxxxxx89</h4>
+                    <h1>How to reach us?</h1>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Email</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    xxxx@xxx.com
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Direct Call</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    91xxxxxx89
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Whatsapp</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    91xxxxxx89
+                                </div>
+                            </div>
+                            <hr/>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Address</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    GT Rd, Gopalmath, Durgapur, West Bengal
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
       </>
