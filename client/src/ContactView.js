133276c46cc4259fd92da2ef32611c89772b113c
Amit Kumar <2292amit@gmail.com>
2021-05-15 13:44:42 +0530

    Adding address in contact

--- client/src/ContactView.js
+++ client/src/ContactView.js
@@ -6,6 +6,7 @@ const ContactView = ({ children }) => {
           <div class="container p-3 my-3 border rounded bg-light text-dark">
                 <div class="text-center">
                 <h1>How to reach us?</h1>
+                <h4>Address: GT Rd, Gopalmath, Durgapur, West Bengal</h4>
                 <h4>Email: xxxx@xxx.com</h4>
                 <h4>Direct Call: 91xxxxxx89</h4>
                 <h4>Whatsapp: 91xxxxxx89</h4>
