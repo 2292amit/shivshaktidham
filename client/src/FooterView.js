eeb0e2c6e4ab922753fe03accf606c7067858ad1
Amit Kumar <2292amit@gmail.com>
2021-05-16 18:23:52 +0530

    Fixing footer

--- client/src/FooterView.js
+++ client/src/FooterView.js
@@ -1,20 +1,39 @@
 import { SocialIcon } from 'react-social-icons';
-import Nav from "react-bootstrap/Nav"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const FooterView = ({ children }) => {
     return (
       <>
-        <div className="navbar navbar-inverse navbar-fixed-bottom">
-            <div className="container">
-            <Nav className="mr-auto">
-                <p className="navbar-text font-italic">Connect with us:  </p>
-                <SocialIcon url="https://twitter.com/shivshakti" />
-                <SocialIcon url="https://www.facebook.com/shivshaktidhamofficial/" />
-            </Nav>
-            <Nav className="ml-auto">
-                <p className="navbar-text">© Shivshakti Dham Trust</p>
-            </Nav>
+            <div className="container" style={{padding:"10px",backgroundColor: "rgb(43, 46, 51)", color: "white"}}>
+              <div className="container" style={{borderBottom: "1px solid white"}}>
+                <Row >
+                  <Col xs={12} sm={6} style={{textAlign: "center"}}>
+                    <h5>Shiv Shakti Dham</h5>
+                    <p>GopalMath, Durgapur, West Bengal</p>
+                  </Col>
+                  <Col xs={12} sm={6} style={{textAlign: "center"}}>
+                    <h5>Founder</h5>
+                    <p>Shri Sambhu Nath Singh</p>
+                  </Col>
+                </Row>
+              </div>
+            <div className="container" style={{marginTop: "10px"}}>
+                <Row >
+                  <Col xs={12} sm={4} style={{textAlign: "center"}}>
+                    <p>© 2021 Shiv Shakti Dham, Durgapur</p>
+                  </Col>
+                  <Col xs={12} sm={4} style={{textAlign: "center"}}>
+                    <p>shivshaktidhamdgr@gmail.com</p>
+                  </Col>
+                  <Col xs={12} sm={4} style={{textAlign: "center"}}>
+                      <SocialIcon url="https://twitter.com/jaketrent" bgColor="transparent" fgColor="white"/>
+                      <SocialIcon url="https://www.facebook.com/shivshaktidhamofficial" bgColor="transparent" fgColor="white"/>
+                      <SocialIcon url="https://youtube.com" bgColor="transparent" fgColor="white"/>
+                      <SocialIcon url="https://instagram.com" bgColor="transparent" fgColor="white"/>
+                  </Col>
+                </Row>
+              </div>
             </div>
-        </div>
       </>
     );
   };
