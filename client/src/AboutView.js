e294a1e9b5fc508662c030a2ae09e02d5d39a729
Amit Kumar <2292amit@gmail.com>
2021-05-16 12:48:58 +0530

    Adding card in about page

--- client/src/AboutView.js
+++ client/src/AboutView.js
@@ -3,12 +3,47 @@
 const AboutView = ({ children }) => {
   return (
     <>
-      <div class="container p-3 my-3 border rounded bg-light text-dark">
-          <div class="text-center">
-          <h4>Welcome to</h4>
-          <h1>Shiv Shakti Dham Temple</h1>
-          <p>Enjoy your time here. We will be back with more content.</p>
+      <div class="card mb-3">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://i.ibb.co/pyJwC5w/1.jpg" class="card-img" alt="..."/>
           </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Location</h5>
+              <p class="card-text">Located in the city of lordess Durga, the temple is located in GopalMath, Durgapur. The temple is easily reachable from Andal airport. </p>
+              <p class="card-text">The view from GT road is mesmerizing and looks even more beautiful in night. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://i.ibb.co/qD1dQXx/7.jpg" class="card-img" alt="..."/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">The Temple</h5>
+              <p class="card-text">Built in XXX acres of land, the temple is one of the biggest temple in West Bengal. </p>
+              <p class="card-text">With lot of open area and ventilation, the temple is one of the best place to visit with family.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card mb-3">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://i.ibb.co/30b6sHg/3.jpg" class="card-img" alt="..."/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Inside the temple</h5>
+              <p class="card-text">Inside the temple there is beautiful statue of Lord Shiva and other hindu Gods.</p>
+              <p class="card-text">There is a big Shiv ling where devotees can offer holy water and worship God.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
